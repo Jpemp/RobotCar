74f86b04024fd4059774f71d9e8ac48784066075
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-11-12 23:30:20 -0600

    Declaring SPI and H-Bridge Switch Variables
    Added SPI communication setup and H-bridge switch configuration.

--- ESP32/main.cpp
+++ ESP32/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPI.h>
 
 // put function declarations here:
 
@@ -8,20 +9,39 @@
 //goForward()
 //goBackward()
 
-//D10, D11, D12, D13 pins are for SPI
+
+//variable declaration for SPI communication
+const int CS = D10; //D10 = CS -- Chip select. Choosing which peripheral to interact with
+const int COPI = D11; //D11 = COPI -- SPI outputs data to peripheral
+const int CIPO = D12; //D12 = CIPO -- SPI inputs data from peripheral
+const int SCLK = D13;  //D13 = SCK -- SPI Serial Clock
+
+//variable declaration for H-bridge switches
+const int SW1 = D2; 
+const int SW2 = D3;
+const int SW3 = D4;
+const int SW4 = D5;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(SCK, OUTPUT);
+  //pin setup as either input or output
+  pinMode(CS, OUTPUT);
+  pinMode(COPI, OUTPUT);
+  pinMode(CIPO, INPUT);
+  pinMode(SCLK, OUTPUT);
 
+  pinMode(SW1, OUTPUT);
+  pinMode(SW2, OUTPUT);
+  pinMode(SW3, OUTPUT);
+  pinMode(SW4, OUTPUT);
+
+  SPI.begin(); //starts SPI communication
+  //Set CS LOW to select peripheral to communicate with and HIGH to de-select and stop communicating
   
 }
 
 void loop() {
-  digitalWrite(SCK, HIGH);
-  delay(1000);
-  digitalWrite(SCK, LOW);
-  delay(1000);
+
 }
 
 // put function definitions here:
