9917d0e31f456694ca6a7ffe0b53da8844191a6e
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-11-20 12:27:21 -0600

    Set up audio datasets and data loader
    Added imports and initialized datasets for audio processing.

--- RaspberryPi/AudioData.py
+++ RaspberryPi/AudioData.py
@@ -1,1 +1,13 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchaudio
+import torchaudio.functional as FA
+import IPython
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
+train_dataset = torchaudio.datasets.SPEECHCOMMANDS(root='C:\\data', download=True, subset="training") #downloads audio data from torchaudio onto the computer
+test_dataset = torchaudio.datasets.SPEECHCOMMANDS(root='C:\\data', download=True, subset="testing") #split audio data into a train and test dataset
+
+data_loader=DataLoader(train_dataset)
