594940c01438efe309ff2a8955056ac0c256585e
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-11-25 15:35:50 -0600

    Implement speech recognition with wav2vec model
    Added functionality for speech recognition using wav2vec model.

--- RaspberryPi/AudioData.py
+++ RaspberryPi/AudioData.py
@@ -1,13 +1,38 @@
 import torch
-from torch import nn
-import torch.nn.functional as F
+from transformers import Speech2TextProcessor
+from datasets import load_dataset
 import torchaudio
-import torchaudio.functional as FA
-import IPython
-import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
 
-train_dataset = torchaudio.datasets.SPEECHCOMMANDS(root='C:\\data', download=True, subset="training") #downloads audio data from torchaudio onto the computer
-test_dataset = torchaudio.datasets.SPEECHCOMMANDS(root='C:\\data', download=True, subset="testing") #split audio data into a train and test dataset
+from torchaudio.utils import _download_asset
 
-data_loader=DataLoader(train_dataset)
+torch.random.manual_seed(0) #change to test reliability of model and debug. This changes the rng of PyTorch functions
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+sample = _download_asset("tutorial-assets/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav") #test sample
+
+bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_100H #a bundle 100 hours of audio dataset for a wav2vec model, and a wav2vec model fine tuned for asr
+
+model = bundle.get_model().to(device) #constructs model based on the bundle (a wav2vec model trained on 100 hours of audio, fine tuned for asr)
+
+waveform, sample_rate = torchaudio.load(sample)
+waveform = waveform.to(device)
+
+if sample_rate != bundle.sample_rate: #to make sure the input sample_rate matches the model sample rate
+    waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
+
+with torch.inference_mode(): #you can use this to look at acoustic features on the transformer layers
+    features, _ = model.extract_features(waveform)
+
+with torch.inference_mode():
+    emission, _ = model(waveform)
+
+print(emission)
+
+class Decoder(torch.nn.Module):
+    def __init__(self, labels, blank):
+        super().__init__()
+        self.labels = labels
+        self.blank = blank
+
+    def forward(self, emission):
+        print("Test")
