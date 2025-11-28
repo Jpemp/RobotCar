6949033ef49fb2d7caf295ba45ff6c9668bdd1a9
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-11-27 20:52:29 -0600

    Add informative comments

--- RaspberryPi/AudioData.py
+++ RaspberryPi/AudioData.py
@@ -30,15 +30,15 @@ print(emission)
 
 class Decoder(torch.nn.Module): #Class for decoder (called Greed
     def __init__(self, labels, blank): #creates a decoder object
-        super().__init__() #calls upon the nn.Module parent class to create the child object
-        self.labels = labels #stores the 
-        self.blank = blank #
+        super().__init__() #calls upon the nn.Module parent class to create the child object that inheirits the parent's attributes
+        self.labels = labels #stores the Label array here
+        self.blank = blank #index that the "-" label is stored in, which indicates dead air/no talking
 
-    def forward(self, emission):
-        indices = torch.argmax(emission, dim=-1)
-        indices = torch.unique_consecutive(indices, dim=-1)
-        indices = [i for i indices if i != self.blank]
-        return "".join([self.labels[i] for i in indices])
+    def forward(self, emission): #this function is called whenever a Decoder object is called after object creation
+        indices = torch.argmax(emission, dim=-1) #in the emission data, stores the index of the highest logit for each frame. the highest logit is what letter is most likely being said in a frame. each logit index corresponds to the Labels array index (ex. 2 for logit index indicates 'E' in label array, which is indedx 2, is the most likely letter being said) 
+        indices = torch.unique_consecutive(indices, dim=-1) #gets rid of consecutive identical numbers in array, which indicates the same letter being said over multiple frames. Gets rid of redundancy
+        indices = [i for i indices if i != self.blank] #gets rid of dead air/"-" indices in the array
+        return "".join([self.labels[i] for i in indices]) #creates a string of words based on indices, with each number in indices corresponding to a character in the labels array. This constructed sentence is the likely sentence that was said in the input audio based on the emission logits
 
 decoder = Decoder(labels=bundle.get_labels(), blank=0) #creates a Decoder object that uses 
 text = decoder(emission[0]) #emission data goes through decoder, outputting text
