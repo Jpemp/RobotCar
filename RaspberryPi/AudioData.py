02706dc3498f2e491e3e3966a41731a44b9f6f3a
Jpemp <159839779+Jpemp@users.noreply.github.com>
2025-11-27 20:31:46 -0600

    Expanding decoder class and added object creation and model test

--- RaspberryPi/AudioData.py
+++ RaspberryPi/AudioData.py
@@ -23,16 +23,24 @@ if sample_rate != bundle.sample_rate: #to make sure the input sample_rate matche
 with torch.inference_mode(): #you can use this to look at acoustic features on the transformer layers
     features, _ = model.extract_features(waveform)
 
-with torch.inference_mode():
+with torch.inference_mode(): #this turns the waveform into a sequence of emissions. An emission is a sequence of logits, which tell the probability that a certain letter is being said in a certain frame of the waveform
     emission, _ = model(waveform)
 
 print(emission)
 
-class Decoder(torch.nn.Module):
-    def __init__(self, labels, blank):
-        super().__init__()
-        self.labels = labels
-        self.blank = blank
+class Decoder(torch.nn.Module): #Class for decoder (called Greed
+    def __init__(self, labels, blank): #creates a decoder object
+        super().__init__() #calls upon the nn.Module parent class to create the child object
+        self.labels = labels #stores the 
+        self.blank = blank #
 
     def forward(self, emission):
-        print("Test")
+        indices = torch.argmax(emission, dim=-1)
+        indices = torch.unique_consecutive(indices, dim=-1)
+        indices = [i for i indices if i != self.blank]
+        return "".join([self.labels[i] for i in indices])
+
+decoder = Decoder(labels=bundle.get_labels(), blank=0) #creates a Decoder object that uses 
+text = decoder(emission[0]) #emission data goes through decoder, outputting text
+
+print(text) #prints out text from decoder to see if its accurate
